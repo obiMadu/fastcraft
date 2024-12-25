17429c45fb1f716bb9d4543315f1b128dc61779f
Jmint <jeffreymintah737@gmail.com>
2024-12-25 16:29:59 +0000

    changes

--- fastcraft/fastcraft/cli/create_dir.py
+++ fastcraft/fastcraft/cli/create_dir.py
@@ -16,38 +16,36 @@ create_dir = typer.Typer()
 
 @create_dir.command()
 def init(projectname: str):
-    base_dir = Path.cwd() / projectname
-    """
-    Start a new FastAPI project 
-    User can choose between SQLAlchemy or SQLModel for database setup.
-    """
-    if base_dir.exists():
-        print(f"[red]❌ Error: A project named '{projectname}' already exists in the current directory.[/red]")
-        raise typer.Exit(code=1)
-    
-    orm_choice = get_orm_choice()
-    steps = [
-        "Creating project folder structure",
-        "Initiating Project and Installing dependencies with uv"
-        
-    ]
-    for step in track(
-        steps, 
-        description="[cyan]✨Creating Project...[/cyan]",    
-    ):
-        try:
-            if step == "Creating project folder structure":
-                generate_file_structure(projectname, orm_choice)
-        
-                
-            if step == "Initiating Project and Installing dependencies with uv":
-                initialize_packages(projectname)
-        except Exception as e:
-            print(f"[red]❌ Failed during {step}: {str(e)}[/red]")
+    try:
+        base_dir = Path.cwd() / projectname
+        """
+        Start a new FastAPI project 
+        User can choose between SQLAlchemy or SQLModel for database setup.
+        """
+        if base_dir.exists():
+            print(f"[red]❌ Error: A project named '{projectname}' already exists in the current directory.[/red]")
             raise typer.Exit(code=1)
         
-        
-         
+        orm_choice = get_orm_choice()
+        steps = [
+            "Creating project folder structure",
+            "Initiating Project and Installing dependencies with uv"
             
-    typer.echo(f"✅ FastAPI project '{projectname}' has been created at {base_dir}")
-    print(f"🎉 Project '{projectname}' is ready!")
+        ]
+        for step in track(
+            steps, 
+            description="[cyan]✨Creating Project...[/cyan]",    
+        ):
+            
+                if step == "Creating project folder structure":
+                    generate_file_structure(projectname, orm_choice)
+            
+                    
+                if step == "Initiating Project and Installing dependencies with uv":
+                    initialize_packages(projectname)
+            
+            
+            
+                
+        typer.echo(f"✅ FastAPI project '{projectname}' has been created at {base_dir}")
+        print(f"🎉 Project '{projectname}' is ready!")
