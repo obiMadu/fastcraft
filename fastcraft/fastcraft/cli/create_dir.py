bec035ee39a1961fb761f3f46517b366fbc78866
Jmint <jeffreymintah737@gmail.com>
2024-12-25 16:24:21 +0000

    changes

--- fastcraft/fastcraft/cli/create_dir.py
+++ fastcraft/fastcraft/cli/create_dir.py
@@ -44,7 +44,7 @@ def init(projectname: str):
             if step == "Initiating Project and Installing dependencies with uv":
                 initialize_packages(projectname)
         except Exception as e:
-            
+            print(f"")
         
         
          
