053d4959da0d18cb9dc79c4349c6d59bac14487f
Jmint <jeffreymintah737@gmail.com>
2024-12-25 14:19:53 +0000

    changes

--- fastcraft/fastcraft/utils/package_manager.py
+++ fastcraft/fastcraft/utils/package_manager.py
@@ -4,7 +4,11 @@ import typer
 
 def initialize_packages(projectname: str):
     try:
-        subprocess.run
+        subprocess.run(
+            [
+                
+            ]
+        )
     except subprocess.CalledProcessError as e:      
         print(f"[red]❌ Error during dependency installation: {e}[/red]")
         raise typer.Exit(code=1)
