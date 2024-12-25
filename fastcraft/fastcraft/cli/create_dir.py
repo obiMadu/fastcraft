1aaa31e36713a3ad1677b32330ba4b7a1f9510ca
Jmint <jeffreymintah737@gmail.com>
2024-12-25 10:49:44 +0000

    ✨Project Intialization

---
+++ fastcraft/fastcraft/cli/create_dir.py
@@ -0,0 +1,28 @@
+import typer
+from rich import print
+from InquirerPy import inquirer
+from fastcraft.utils.generate_file_struct import generate_file_structure
+
+
+create_dir = typer.Typer()
+
+
+@create_dir.command()
+def init(projectname: str):
+    """
+    Start a new FastAPI project 
+    User can choose between SQLAlchemy or SQLModel for database setup.
+    """
+    print(f"🚀 Starting new project: {projectname}")
+    orm_choice = inquirer.select(
+        message  = "Select the database ORM to use:",
+        choices = [
+            {'name': "SQLAlchemy", "value": "sqlalchemy"},
+            {'name': "SQLModel", "value": "sqlmodel"},
+        ],
+        default ="sqlmodel"
+        
+    ).execute()
+        
+    generate_file_structure(projectname, orm_choice)
+    print(f"🎉 Project '{projectname}' is ready!")
---
+++ fastcraft/fastcraft/utils/generate_file_struct.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import os
+
+def generate_file_structure(projectname: str, orm_choice: str):
+    """
+    Generate a basic folder structure for a FastAPI project, considering cross-platform compatibility.
+    """
+    base_dir = Path.cwd() / projectname
+
+    # Define the folder structure
+    folders = [
+        "app",
+        "app/routers",
+        "app/models",
+        "app/core",
+        "app/utils",
+        "tests"
+    ]
+    
+    if orm_choice == 'sqlalchemy':
+        schemas_file_path = base_dir / "app" / "schemas" / "schemas.py"
+        schemas_file_path.write_text(
+            """from pydantic import BaseModel\n\n"""
+            """class Item(BaseModel):\n"""
+            """    name: str\n"""
+            """    description: str = None\n""",
+            encoding="utf-8"
+        )
+        
+
+    # Create the directories
+    for folder in folders:
+        dir_path = base_dir / folder
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+    # Create a basic main.py file
+    
+    main_file_path = base_dir / "app" / "main.py"
+    main_file_path.write_text(
+        """from fastapi import FastAPI\n\n"""
+        """app = FastAPI()\n\n"""
+        """@app.get("/")\n"""
+        """def read_root():\n"""
+        """    return {"message": "Hello, FastForge!"}\n""",
+        encoding="utf-8"
+    )
+
+    print(f"✅ Basic FastAPI project '{projectname}' has been created at {base_dir}")
