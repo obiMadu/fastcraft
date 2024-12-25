517e2ad6c81cc09a0888a484a314ce209ab786b0
Jmint <jeffreymintah737@gmail.com>
2024-12-25 11:32:39 +0000

    changes

--- fastcraft/fastcraft/utils/choices.py
+++ fastcraft/fastcraft/utils/choices.py
@@ -1,10 +1,12 @@
 
-orm_choice = inquirer.select(
-        message  = "Select the database ORM to use:",
-        choices = [
-            {'name': "SQLAlchemy", "value": "sqlalchemy"},
-            {'name': "SQLModel", "value": "sqlmodel"},
-        ],
-        default ="sqlmodel",
-        
-    ).execute()
+
+def get_orm_choice():
+    orm_choice = inquirer.select(
+            message  = "Select the database ORM to use:",
+            choices = [
+                {'name': "SQLAlchemy", "value": "sqlalchemy"},
+                {'name': "SQLModel", "value": "sqlmodel"},
+            ],
+            default ="sqlmodel",
+            
+        ).execute()
