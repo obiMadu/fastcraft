d09fa81f85963f4250c048c1a959b43569edac6d
Jmint <jeffreymintah737@gmail.com>
2024-12-25 16:48:21 +0000

    changes

--- fastcraft/fastcraft/utils/generate_file_struct.py
+++ fastcraft/fastcraft/utils/generate_file_struct.py
@@ -1,5 +1,3 @@
-import typer
-from rich import print
 from pathlib import Path
 import os
 from fastcraft.utils.file_writes import write_schemas_file, write_main_file
@@ -9,39 +7,35 @@ def generate_file_structure(projectname: str, orm_choice: str):
     """
     Generate a basic folder structure for a FastAPI project, considering cross-platform compatibility.
     """
-    try:
-        base_dir = Path.cwd() / projectname
+    base_dir = Path.cwd() / projectname
 
-        # Define the folder structure
-        folders = [
-            "app",
-            "app/routers",
-            "app/models",
-            "app/core",
-            "app/utils",
-            "tests"
-        ]
+    # Define the folder structure
+    folders = [
+        "app",
+        "app/routers",
+        "app/models",
+        "app/core",
+        "app/utils",
+        "tests"
+    ]
+    
+    # If ORM choice given by the user is SQLAlchemy, include the shemas folder in the scaffold
+    if orm_choice == 'sqlalchemy':
+       folders.append("app/schemas")
         
-        # If ORM choice given by the user is SQLAlchemy, include the shemas folder in the scaffold
-        if orm_choice == 'sqlalchemy':
-            folders.append("app/schemas")
-            
-        # Create the directories
-        for folder in folders:
-            dir_path = base_dir / folder
-            dir_path.mkdir(parents=True, exist_ok=True)
+    # Create the directories
+    for folder in folders:
+        dir_path = base_dir / folder
+        dir_path.mkdir(parents=True, exist_ok=True)
+
+  
+    # prefill the schemas.py with some useful information 
+    if orm_choice == 'sqlalchemy':
+        write_schemas_file(projectname)
+        
+    # Create a basic main.py file
+    write_main_file(projectname)
 
     
-        # prefill the schemas.py with some useful information 
-        if orm_choice == 'sqlalchemy':
-            write_schemas_file(projectname)
-            
-        # Create a basic main.py file
-        write_main_file(projectname)
-    except Exception as e:
-        print(f"[red]❌ Error: {e}[/red]")
-        raise typer.Exit(code=1)
-
-        
-        
+    
 
