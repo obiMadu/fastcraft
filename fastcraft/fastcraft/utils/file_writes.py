30f900434cb45ba53f1d525dff988ac8cd5011b1
Jmint <jeffreymintah737@gmail.com>
2024-12-25 12:15:16 +0000

    ♻️Refactor codes for clean architecture

--- fastcraft/fastcraft/utils/file_writes.py
+++ fastcraft/fastcraft/utils/file_writes.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 
-
 def write_schemas_file(projectname: str):
     base_dir = Path.cwd() / projectname
     schemas_file = base_dir / "app" / "schemas" / "schemas.py"
@@ -16,3 +15,19 @@ def write_schemas_file(projectname: str):
     )
     
     return schemas_file
+
+def write_main_file(projectname):
+    base_dir = Path.cwd() / projectname
+    main_file = base_dir / "app" / "main.py"
+    main_file.parent.mkdir(parents=True, exist_ok=True)
+    
+    main_file.write_text(
+        """from fastapi import FastAPI\n\n"""
+        """app = FastAPI()\n\n"""
+        """@app.get("/")\n"""
+        """def read_root():\n"""
+        """    return {"message": "Hello, FastForge!"}\n""",
+        encoding="utf-8"
+    )
+    
+    return main_file
--- fastcraft/fastcraft/utils/generate_file_struct.py
+++ fastcraft/fastcraft/utils/generate_file_struct.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-from fastcraft.utils.file_writes import write_schemas_file
+from fastcraft.utils.file_writes import write_schemas_file, write_main_file
 
 def generate_file_structure(projectname: str, orm_choice: str):
     """
@@ -33,15 +33,7 @@ def generate_file_structure(projectname: str, orm_choice: str):
         write_schemas_file(projectname)
         
     # Create a basic main.py file
-    main_file_path = base_dir / "app" / "main.py"
-    main_file_path.write_text(
-        """from fastapi import FastAPI\n\n"""
-        """app = FastAPI()\n\n"""
-        """@app.get("/")\n"""
-        """def read_root():\n"""
-        """    return {"message": "Hello, FastForge!"}\n""",
-        encoding="utf-8"
-    )
+    write_main_file(projectname)
 
     print(f"✅ Basic FastAPI project '{projectname}' has been created at {base_dir}")
     
