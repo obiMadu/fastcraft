02ff943f0b1177200774effccdac1dbd68230edb
Jmint <jeffreymintah737@gmail.com>
2024-12-25 11:56:28 +0000

    changes

--- fastcraft/fastcraft/utils/file_writes.py
+++ fastcraft/fastcraft/utils/file_writes.py
@@ -1,3 +1,15 @@
 from pathlib import Path
 
 base_dir = Path.cwd() / projectname
+
+def schemas_file_path():
+    schemas_file = base_dir / "app" / "schemas" / "schemas.py"
+    schemas_file.write_text(
+        """from pydantic import BaseModel\n\n"""
+        """class Item(BaseModel):\n"""
+        """    name: str\n"""
+        """    description: str = None\n""",
+        encoding="utf-8"
+    )
+    
+    return schemas_file
