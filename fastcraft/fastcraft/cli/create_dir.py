a62e2d0df45f6b8dfd447a00262bdc71e7628eb1
Jmint <jeffreymintah737@gmail.com>
2024-12-25 13:12:56 +0000

    changes

--- fastcraft/fastcraft/cli/create_dir.py
+++ fastcraft/fastcraft/cli/create_dir.py
@@ -3,6 +3,7 @@ import time
 from rich import print
 from rich.progress import track
 from InquirerPy import get_style
+from pathlib import Path
 from fastcraft.utils.generate_file_struct import generate_file_structure
 from fastcraft.utils.choices import get_orm_choice
 
@@ -12,6 +13,7 @@ create_dir = typer.Typer()
 
 @create_dir.command()
 def init(projectname: str):
+    base_dir = Path.cwd() / projectname
     """
     Start a new FastAPI project 
     User can choose between SQLAlchemy or SQLModel for database setup.
@@ -25,5 +27,5 @@ def init(projectname: str):
      
         if step == "Creating project folder structure":
             generate_file_structure(projectname, orm_choice)
-
+    typer.echo(f"✅ Basic FastAPI project '{projectname}' has been created at {base_dir}")
     print(f"🎉 Project '{projectname}' is ready!")
